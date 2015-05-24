4d57181c9e9977a55384a8d25fd81ff6f0422b69
Fotis Gimian <fgimiansoftware@gmail.com>
2015-05-24 16:17:38 +1000

    Major re-structure of the netsnmp client which has now been split into easy, session and variables modules

--- netsnmp/__init__.py
+++ netsnmp/__init__.py
@@ -1,4 +1,5 @@
-from client import (
-    Varbind, VarList, Session,
+from .easy import (
     snmpget, snmpset, snmpgetnext, snmpgetbulk, snmpwalk
 )  # flake8: noqa
+from .session import Session  # flake8: noqa
+from .variables import Varbind, VarList  # flake8: noqa
---
+++ netsnmp/variables.py
@@ -0,0 +1,65 @@
+import re
+
+
+def tostr(s):
+    """Converts any variable to a string"""
+    return None if s is None else str(s)
+
+
+class Varbind(object):
+    def __init__(self, tag=None, iid=None, val=None, type=None):
+        self.tag = tostr(tag)
+        self.iid = tostr(iid)
+        self.val = tostr(val)
+        self.type = tostr(type)
+        # parse iid out of tag if needed
+        if iid is None and tag is not None:
+            regex = re.compile(r'^((?:\.\d+)+|(?:\w+(?:[-:]*\w+)+))\.?(.*)$')
+            match = regex.match(tag)
+            if match:
+                (self.tag, self.iid) = match.group(1, 2)
+
+    def __setattr__(self, name, val):
+        self.__dict__[name] = tostr(val)
+
+
+class VarList(object):
+    def __init__(self, *vs):
+        self.varbinds = []
+
+        for var in vs:
+            if isinstance(var, Varbind):
+                self.varbinds.append(var)
+            else:
+                self.varbinds.append(Varbind(var))
+
+    def __len__(self):
+        return len(self.varbinds)
+
+    def __getitem__(self, index):
+        return self.varbinds[index]
+
+    def __setitem__(self, index, val):
+        if isinstance(val, Varbind):
+            self.varbinds[index] = val
+        else:
+            raise TypeError
+
+    def __iter__(self):
+        return iter(self.varbinds)
+
+    def __delitem__(self, index):
+        del self.varbinds[index]
+
+    def __repr__(self):
+        return repr(self.varbinds)
+
+    def __getslice__(self, i, j):
+        return self.varbinds[i:j]
+
+    def append(self, *vars):
+        for var in vars:
+            if isinstance(var, Varbind):
+                self.varbinds.append(var)
+            else:
+                raise TypeError
