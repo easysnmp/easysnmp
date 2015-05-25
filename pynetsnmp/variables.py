c0f334dbbb2dce4dd16f06c92a877469f283af65
Fotis Gimian <fgimiansoftware@gmail.com>
2015-05-25 23:49:59 +1000

    Simplified the code for the VarList class

--- pynetsnmp/variables.py
+++ pynetsnmp/variables.py
@@ -13,7 +13,7 @@ class Varbind(object):
         self.val = tostr(val)
         self.type = tostr(type)
 
-        # Parse iid out of tag if needed
+        # Parse index id out of tag if needed
         if iid is None and tag is not None:
             regex = re.compile(r'^((?:\.\d+)+|(?:\w+(?:[-:]*\w+)+))\.?(.*)$')
             match = regex.match(tag)
@@ -24,43 +24,12 @@ class Varbind(object):
         self.__dict__[name] = tostr(val)
 
 
-class VarList(object):
-    def __init__(self, *vs):
-        self.varbinds = []
+class VarList(list):
+    """An slight variation of a list which is used internally by the
+    Net-SNMP C interface"""
+    def __init__(self, *items):
+        self.extend(items)
 
-        for var in vs:
-            if isinstance(var, Varbind):
-                self.varbinds.append(var)
-            else:
-                self.varbinds.append(Varbind(var))
-
-    def __len__(self):
-        return len(self.varbinds)
-
-    def __getitem__(self, index):
-        return self.varbinds[index]
-
-    def __setitem__(self, index, val):
-        if isinstance(val, Varbind):
-            self.varbinds[index] = val
-        else:
-            raise TypeError
-
-    def __iter__(self):
-        return iter(self.varbinds)
-
-    def __delitem__(self, index):
-        del self.varbinds[index]
-
-    def __repr__(self):
-        return repr(self.varbinds)
-
-    def __getslice__(self, i, j):
-        return self.varbinds[i:j]
-
-    def append(self, *vars):
-        for var in vars:
-            if isinstance(var, Varbind):
-                self.varbinds.append(var)
-            else:
-                raise TypeError
+    @property
+    def varbinds(self):
+        return self
