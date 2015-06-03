d41cbd9763f8e7f8e791c580b1de9285ac61db7b
Fotis Gimian <fgimiansoftware@gmail.com>
2015-06-03 13:42:24 +1000

    Incremented version to 0.2.2 for release

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ except IOError:
 
 setup(
     name='easysnmp',
-    version='0.2.1',
+    version='0.2.2',
     description='A blazingly fast and Pythonic SNMP library based on the '
                 'official Net-SNMP bindings',
     long_description=long_description,
