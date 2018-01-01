c1720e7f3cbeeeb5aad36442bc4412c290c45e20
ccdtolle <charles_tolley@byu.edu>
2018-01-01 14:16:57 -0700

    Replaced buggy regex in helpers.py by using string.split

--- easysnmp/helpers.py
+++ easysnmp/helpers.py
@@ -1,22 +1,5 @@
 from __future__ import unicode_literals
 
-import re
-
-# This regular expression is used to extract the index from an OID
-OID_INDEX_RE = re.compile(
-    r'''(
-            \.?\d+(?:\.\d+)*              # numeric OID
-            |                             # or
-            (?:\w+(?:[-:]*\w+)+)          # regular OID
-            |                             # or
-            (?:\.?iso(?:\.\w+[-:]*\w+)+)  # fully qualified OID
-        )
-        \.?(.*)                           # OID index
-     ''',
-    re.VERBOSE
-)
-
-
 def normalize_oid(oid, oid_index=None):
     """
     Ensures that the index is set correctly given an OID definition.
@@ -29,8 +12,9 @@ def normalize_oid(oid, oid_index=None):
     if oid_index is None and oid is not None:
         # We attempt to extract the index from an OID (e.g. sysDescr.0
         # or .iso.org.dod.internet.mgmt.mib-2.system.sysContact.0)
-        match = OID_INDEX_RE.match(oid)
-        if match:
-            oid, oid_index = match.group(1, 2)
+        subidentifiers = oid.split('.')
+        if subidentifiers:
+            oid_index = subidentifiers.pop()
+            oid = '.'.join(subidentifiers)
 
     return oid, oid_index
