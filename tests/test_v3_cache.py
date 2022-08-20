d8d60ac0419a0b596a205816b676ea4a44f3adbe
Kent Coble <coblekent@gmail.com>
2022-08-20 13:39:47 -0500

    Update Python files to Black formatting specifications

--- tests/test_v3_cache.py
+++ tests/test_v3_cache.py
@@ -7,7 +7,7 @@ from easysnmp.exceptions import EasySNMPConnectionError
 
 def test_v3_not_caching_user(sess_v3):
     s = Session(**sess_v3)
-    res = s.get('sysDescr.0')
+    res = s.get("sysDescr.0")
 
     assert res.oid == "sysDescr"
     assert res.oid_index == "0"
@@ -15,7 +15,7 @@ def test_v3_not_caching_user(sess_v3):
     s.update_session(privacy_password="wrong_pass")
 
     with pytest.raises(EasySNMPConnectionError):
-        res = s.get('sysDescr.0')
+        res = s.get("sysDescr.0")
 
     d = dict(**sess_v3)
     d["privacy_password"] = "wrong_pass"
@@ -24,7 +24,7 @@ def test_v3_not_caching_user(sess_v3):
         res = s.get("sysDescr.0")
 
     s.update_session(privacy_password="priv_pass")
-    res = s.get('sysDescr.0')
+    res = s.get("sysDescr.0")
 
     assert res.oid == "sysDescr"
     assert res.oid_index == "0"
