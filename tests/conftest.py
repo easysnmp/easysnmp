8355f7613e0af1fb4ba2f1cc0ee6d1327039dedf
Carlos <27721404+carlkid1499@users.noreply.github.com>
2023-03-04 20:13:28 -0800

    Update conftest.py
    -- flake8 and adding back "pass" and def

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,34 +8,39 @@ from subprocess import Popen, DEVNULL
 import easysnmp
 
 assert version_info[0] == 3 and version_info[1] >= 8
+
+
 class SNMPSetCLIError(Exception):
     """An exception raised when an SNMP SET fails via the CLI."""
 
-    def snmp_set_via_cli(oid, value, type):
-        """
-        Sets an SNMP variable using the snmpset command.
+    pass
 
-        :param oid: the OID to update
-        :param value: the new value to set the OID to
-        :param type: a single character type as required by the snmpset command
-                     (i: INTEGER, u: unsigned INTEGER, t: TIMETICKS,
-                      a: IPADDRESS o: OBJID, s: STRING, x: HEX STRING,
-                      d: DECIMAL STRING, b: BITS U: unsigned int64,
-                      I: signed int64, F: float, D: double)
-        """
-        process = Popen(
-            "snmpset -v2c -c public localhost:11161 {} {} {}".format(
-                oid, type, '"{}"'.format(value) if type == "s" else value
-            ),
-            stdout=DEVNULL,
-            stderr=DEVNULL,
-            shell=True,
+
+def snmp_set_via_cli(oid, value, type):
+    """
+    Sets an SNMP variable using the snmpset command.
+
+    :param oid: the OID to update
+    :param value: the new value to set the OID to
+    :param type: a single character type as required by the snmpset command
+                    (i: INTEGER, u: unsigned INTEGER, t: TIMETICKS,
+                    a: IPADDRESS o: OBJID, s: STRING, x: HEX STRING,
+                    d: DECIMAL STRING, b: BITS U: unsigned int64,
+                    I: signed int64, F: float, D: double)
+    """
+    process = Popen(
+        "snmpset -v2c -c public localhost:11161 {} {} {}".format(
+            oid, type, '"{}"'.format(value) if type == "s" else value
+        ),
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+        shell=True,
+    )
+    process.communicate()
+    if process.returncode != 0:
+        raise SNMPSetCLIError(
+            "failed to set {0} to {1} (type {2})".format(oid, value, type)
         )
-        process.communicate()
-        if process.returncode != 0:
-            raise SNMPSetCLIError(
-                "failed to set {0} to {1} (type {2})".format(oid, value, type)
-            )
 
 
 # Disable logging for the C interface
